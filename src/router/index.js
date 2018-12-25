b59b641505683c15eca0930ac77f8a7f4979dd3a
Isabelleqqq <757489443@qq.com>
2018-12-25 21:18:13 +0800

    '登录功能完成'

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
 // @表示src的绝对路径
 import Home from '@/components/home'
 import Login from '@/components/login'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
-      redirect: '/'
+      redirect: '/login'
     },
     {
       path: '/login',
@@ -23,3 +22,23 @@ export default new Router({
     }
   ]
 })
+
+// 给router对象注册导航守卫
+router.beforeEach((to, from, next) => {
+  // 如果去登陆，就放行
+  if (to.path === '/login') {
+    next()
+    return
+  }
+  // 如果不是去登陆，先判断是否有token，如果有，放走
+  // 如果没有转去登录
+  let token = localStorage.getItem('token')
+  if (token) {
+    // 不拦截
+    next()
+  } else {
+    // 跳转到登录
+    next('/login')
+  }
+})
+export default router
